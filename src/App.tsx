c7b268af287373d40b74dfbd8901821ecb51d455
kianeblackman <kianeblackman@gmail.com>
2024-06-25 13:16:38 +0100

    pages, components, hooks and assets folders made and initeal routes set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+// Import statements
+import { Route, Routes } from 'react-router-dom';
+
+// Import pages
+import Home from './pages/Home.tsx';
+import NotFound from './pages/NotFound.tsx';
+
+// Import stlyes
 import './scss/styles.css';
 
 function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </div>
   );
 }
 

6e6876e0398e16a7a7ea5da0cba7de3d8d509d6c
kianeblackman <kianeblackman@gmail.com>
2024-07-09 17:01:21 +0100

    scope added to hero animation

--- src/pages/home/Hero.tsx
+++ src/pages/home/Hero.tsx
@@ -6,8 +6,7 @@ gsap.registerPlugin(useGSAP);
 
 function Hero() {
   const [time, setTime] = useState(new Date());
-  const kianeRef = useRef(null);
-  const blackmanRef = useRef(null);
+  const container = useRef(null);
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -27,30 +26,31 @@ function Hero() {
 
   useGSAP(() => {
     gsap.fromTo(
-      kianeRef.current,
+      ".kiane",
       { opacity: 0, y: 50 },
       { opacity: 1, y: 0, duration: 2, ease: "power4.easeInOut", delay: 1 }
     );
 
     gsap.fromTo(
-      blackmanRef.current,
+      ".blackman",
       { opacity: 0, y: 50 },
       { opacity: 1, y: 0, duration: 2, ease: "power4.easeInOut", delay: 0.5 }
-    );
+    ),
+      { scope: container };
   });
 
   return (
     <>
       <h2 className="time">London, {ukTime}</h2>
-      <h1 className="home_hero heading">
-        <span className="home_hero kiane" ref={kianeRef}>
+      <h1 className="home_hero heading" ref={container}>
+        <span className="home_hero kiane">
           <span className="k">K</span>
           <span className="i">I</span>
           <span className="a">A</span>
           <span className="n">N</span>
           <span className="e">E</span>
         </span>
-        <span className="home_hero blackman" ref={blackmanRef}>
+        <span className="home_hero blackman">
           <span className="b">B</span>
           <span className="l">L</span>
           <span className="a">A</span>

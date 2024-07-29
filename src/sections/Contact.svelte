b2069381a16ca1f85dc44f06cc9d39f32e728d60
kianeblackman <kianeblackman@gmail.com>
2024-07-29 22:35:05 +0100

    contact section heading added

---
+++ src/sections/Contact.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<section id="contact">
+	<div class="heading">
+		<div class="container flex align-center space-between">
+			<h2>COTACT</h2>
+			<h3>SCROLL TO TOP</h3>
+		</div>
+	</div>
+</section>

dde82c016a0aaf862515b0fdc7d53ff2c375eb55
kianeblackman <kianeblackman@gmail.com>
2024-07-29 20:24:33 +0100

    hero clean up

--- src/sections/Hero.svelte
+++ src/sections/Hero.svelte
@@ -2,16 +2,22 @@
 	import { waveyUnderline, heroGraphic } from '../lib';
 </script>
 
-<section id="hero" class="container">
-	<h1 class="regular hero-text">
-		<span class="bold">Kiane Blackman</span>🡢 a curious<br />
-		and creative
-		<span class="underline-wrapper">
-			web developer
-			<div class="underline" style="--underline-url: url('{waveyUnderline}');"></div>
-		</span>
-		<br />
-		with an eye for design!
-		<span class="sticker">PORTFOLIO 2024</span>
-	</h1>
+<section id="hero" class="hero">
+	<div class="container">
+		<h1 class="fw-regular hero-text">
+			<div class="bold">Kiane Blackman</div>
+			🡢 a curious and creative
+			<div class="underline-wrapper">
+				web developer
+				<div class="underline" style="--underline-url: url('{waveyUnderline}');"></div>
+			</div>
+			with an eye for design!
+			<div class="sticker">
+				<h3 class="fw-regular">PORTFOLIO 2024</h3>
+			</div>
+		</h1>
+		<!-- <div class="hero-graphic">
+			<img src={heroGraphic} alt="Hero Graphic" />
+		</div> -->
+	</div>
 </section>

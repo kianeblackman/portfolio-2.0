ad1252df72b94f49ae8153957a10bad3a90a7a37
kianeblackman <kianeblackman@gmail.com>
2024-07-29 11:17:46 +0100

    sass files added and images set up in lib folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+	import { heroGraphic } from '../lib';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<img src={heroGraphic} alt="SvelteKit logo" />

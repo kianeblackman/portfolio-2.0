55b6fd92345f6882889a982f680f9b59508c468d
kianeblackman <kianeblackman@gmail.com>
2024-08-06 09:44:26 +0100

    about section css clearned up

--- src/layout/About.svelte
+++ src/layout/About.svelte
@@ -7,7 +7,7 @@
 	let learningStack: string[] = [];
 
 	onMount(async () => {
-		const response = await fetch('/data/tech-stack.json');
+		const response = await fetch('/data/techstack.json');
 		const data = await response.json();
 		hardStack = data.hardStack;
 		softStack = data.softStack;
@@ -19,64 +19,64 @@
 	<Heading customClass="heading--orange" heading="About" />
 	<div class="about__content container section even-columns">
 		<div class="about__main">
-			<h2 class="fw-regular">
+			<h2 class="about__introduction fw-regular">
 				Hello there! My name is Kiane Blackman. I'm a creative web developer based in the South of
 				England with an eye for design! Aside from creating digital stuff I like gardening, cooking,
 				swimming and many more outdoor activities.
 			</h2>
-			<div class="about__aditional even-columns">
+			<div class="about__additional">
 				<div class="about__card about__card--position">
-					<div class="about__card__item">
+					<div class="about__card-item">
 						<h6>Position</h6>
 						<p>Jnr. Web Developer</p>
 					</div>
-					<div class="about__card__item">
+					<div class="about__card-item">
 						<h6>Experience</h6>
 						<p>2+ years</p>
 					</div>
-					<div class="about__card__item">
+					<div class="about__card-item">
 						<h6>Current Company</h6>
 						<p class="d-flex align-items-center">Cormack Advertising</p>
 					</div>
-					<div class="about__card__item">
+					<div class="about__card-item">
 						<h6>Degree</h6>
 						<p class="mb-0">First in Graphic Design</p>
 					</div>
 				</div>
 				<div class="about__card about__card--contact">
-					<div class="about__card__item">
+					<div class="about__card-item">
 						<h6>Email</h6>
 						<p>hello@gimaev.com</p>
 					</div>
-					<div class="about__card__item">
-						<h6>LOCATION</h6>
+					<div class="about__card-item">
+						<h6>Location</h6>
 						<p>Eastbourne and London</p>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="about__tech-stack">
-			<div class="tech-stack__category">
-				<h3 class="tech-stack__heading">Hard Stack</h3>
-				<ul class="tech-stack__list">
+		<div class="about__techstack">
+			<div class="about__techstack-category">
+				<h3 class="about__techstack-heading">Hard Stack</h3>
+				<ul class="about__techstack-list">
 					{#each hardStack as tech}
-						<li class="tech-stack__sticker">{tech}</li>
+						<li class="about__techstack-sticker">{tech}</li>
 					{/each}
 				</ul>
 			</div>
-			<div class="tech-stack__category">
-				<h3 class="tech-stack__heading">Soft Stack</h3>
-				<ul class="tech-stack__list">
+			<div class="about__techstack-category">
+				<h3 class="about__techstack-heading">Soft Stack</h3>
+				<ul class="about__techstack-list">
 					{#each softStack as tech}
-						<li class="tech-stack__sticker">{tech}</li>
+						<li class="about__techstack-sticker">{tech}</li>
 					{/each}
 				</ul>
 			</div>
-			<div class="tech-stack__category">
-				<h3 class="tech-stack__heading">Learning Stack</h3>
-				<ul class="tech-stack__list">
+			<div class="about__techstack-category">
+				<h3 class="about__techstack-heading">Learning Stack</h3>
+				<ul class="about__techstack-list">
 					{#each learningStack as tech}
-						<li class="tech-stack__sticker">{tech}</li>
+						<li class="about__techstack-sticker">{tech}</li>
 					{/each}
 				</ul>
 			</div>
